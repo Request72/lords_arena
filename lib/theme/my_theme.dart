b977590646a1d628cf3cbcbd0286ed8ac3167970
Request72 <maharjanrequest@gmail.com>
2025-05-31 15:25:44 +0545

    theme and font added

---
+++ lib/theme/my_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    primarySwatch: Colors.deepOrange,
+    scaffoldBackgroundColor: Colors.grey,
+    fontFamily: "OpenSansItalic",
+  );
+}
